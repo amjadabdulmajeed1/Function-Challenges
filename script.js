563daa0ee6b6256fbe5de0880e5e72f3f2067389
Amjad <amjad@gmail.com>
2025-01-08 12:35:25 +0100

    Challenge 2

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 //Challenge 1
 const getCelsius = (f) => ((f - 32) * 5) / 9;
 console.log(`The temp is ${getCelsius(32)} \xB0C `);
+
+// Challenge 2
+function minMax(arr) {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+
+  return {
+    min,
+    max,
+  };
+}
+
+console.log(minMax([2, 31, 4, 5, 6]));
